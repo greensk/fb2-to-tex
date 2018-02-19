fd295cedc5fc4463f9add8be681b9f715f20fe01
Grigory Kosheev <grigory@kosheev.com>
2018-02-19 14:33:56 +0700

    feat set file output

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Convert FB2 file to TeX')
 parser.add_argument('input', metavar='input_file', type=str, help='File to read (.fb2 or .fb2.zip)')
-parser.add_argument('output', metavar='output_file', nargs='?', type=str, help='Output file (.tex)')
+parser.add_argument('output', metavar='output_file', nargs='?', type=str, help='Output file (.tex), set - for stdout')
 
 parser.add_argument('--paperwidth', type=float, help='Output document width (cm)', default=7.0)
 parser.add_argument('--paperheight', type=float, help='Output document height (cm)', default=10.0)
@@ -39,6 +39,18 @@ tex.lmargin = argsValues['lmargin']
 
 tex.lang = argsValues['lang']
 
+output = None
+outputFilename = argsValues['output']
+
+if outputFilename == None:
+    outputFilename = '%s.tex' % fb.getFileName()
+    
+if outputFilename != '-':
+    output = open(outputFilename, 'w')
+    sys.stdout = output
 
 parser = etree.XMLParser(target = Converter(tex))
 result = etree.XML(fb.getFileContent(), parser)
+
+if output:
+    output.close()
